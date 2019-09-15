e0009ef74bae9ccd27731e7297c16c9cff2e9b79
yuta-tsunemoto <tsunemoto@est.co.jp>
2019-09-15 18:58:53 +0900

    SEARCH_UI_COMPETITION-15  Add sample files.

---
+++ src/Project/Project.SampleWebSite/Controllers/SearchPageController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace Project.SampleWebSite.Controllers
+{
+    public class SearchPageController : Controller
+    {
+        public ActionResult SearchPage()
+        {
+            return View();
+        }
+    }
+}
