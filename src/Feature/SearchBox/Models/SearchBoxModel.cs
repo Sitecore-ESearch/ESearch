431c74bca9fa23b64c6296bbf6cc00889d39f90f
yuta-tsunemoto <tsunemoto@est.co.jp>
2019-10-16 16:22:29 +0900

    Added doc comments to search box component

--- src/Feature/SearchBox/Models/SearchBoxModel.cs
+++ src/Feature/SearchBox/Models/SearchBoxModel.cs
@@ -3,12 +3,29 @@ using Sitecore.Data.Items;
 
 namespace ESearch.Feature.SearchBox.Models
 {
+    /// <summary>
+    /// data of the search box component
+    /// </summary>
     public class SearchBoxModel
     {
+        /// <summary>
+        /// property for accessing search keywords
+        /// </summary>
         public string Keyword { get; set; }
+
+        /// <summary>
+        /// property for accessing Item ID of the search setting item set in the rendering parameter
+        /// </summary>
         public string SearchSettingsItemId { get; set; }
+
+        /// <summary>
+        /// property for accessing List of search result items
+        /// </summary>
         public ICollection<Item> Items { get; set; }
 
+        /// <summary>
+        /// constructor
+        /// </summary>
         public SearchBoxModel()
         {
         }        
