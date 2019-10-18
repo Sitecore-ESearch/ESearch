8e2b1c906ca32f22eb81d6f1d9329984030c6235
ayane-suzuki <a-suzuki@est.co.jp>
2019-10-18 15:33:31 +0900

    SEARCH_UI_COMPETITION-10 Output the total count and format a search query

--- src/Feature/SearchResultSummary/Models/SearchResultSummaryModel.cs
+++ src/Feature/SearchResultSummary/Models/SearchResultSummaryModel.cs
@@ -1,49 +1,83 @@
-using System.Collections.Generic;
-using ESearch.Foundation.SitecoreExtensions.Extensions;
-using ESearch.Foundation.Indexing;
-using ESearch.Foundation.Indexing.Models;
-using Sitecore;
-using Sitecore.Data.Items;
 using System;
-using System.Globalization;
 using System.Linq;
+using System.Collections.Generic;
+using Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using SearchSettingsTemplate = ESearch.Foundation.Indexing;
+using ESearch.Foundation.Indexing.Models;
 
 namespace ESearch.Feature.SearchResultSummary.Models
 {
     public class SearchResultSummaryModel
     {
         #region Property
-
         public string Keywords { get; set; }
 
+        public int TotalCount { get; set; }
+
         public SearchQuery SearchQuery { get; set; }
-
-        public ICollection<BetweenCondition> BetweenConditions { get; set; }
-
+        public bool IsDisplayConditions { get; set; }
         #endregion
 
         #region Constructor
-        public SearchResultSummaryModel(SearchQuery searchQuery, Item searchSettings)
+        public SearchResultSummaryModel(SearchQuery searchQuery, Item searchSettings, int totalCount)
         {
             SearchQuery = searchQuery;
-
+            TotalCount = totalCount;
             Keywords = string.Join(", ", SearchQuery.KeywordCondition.Keywords);
+            IsDisplayConditions = !string.IsNullOrEmpty(Keywords)
+                                  || SearchQuery.EqualsConditions.Count != 0
+                                  || SearchQuery.ContainsConditions.Count != 0
+                                  || SearchQuery.BetweenConditions.Count != 0;
 
-            BetweenConditions = SearchQuery.BetweenConditions;
-
-            var dateFormat = searchSettings[Templates.SearchSettings.Fields.DateFormat];
-            
-            foreach (var betweensCondition in BetweenConditions ?? Enumerable.Empty<BetweenCondition>())
+            if (IsDisplayConditions)
             {
-                betweensCondition.LowerValue = DateTime.TryParse(betweensCondition.LowerValue, out var lowerDate)
-                    ? lowerDate.ToLocalTime().ToString(dateFormat)
-                    : betweensCondition.LowerValue;
-                betweensCondition.UpperValue = DateTime.TryParse(betweensCondition.UpperValue, out var upperDate)
-                    ? upperDate.ToLocalTime().ToString(dateFormat)
-                    : betweensCondition.UpperValue;
+                var dateFormat = searchSettings[SearchSettingsTemplate.Templates.SearchSettings.Fields.DateFormat];
+
+                foreach (var equalCondition in SearchQuery.EqualsConditions ?? Enumerable.Empty<EqualsCondition>())
+                {
+                    equalCondition.Value = GetValue(equalCondition.Value, dateFormat);
+                }
+
+                foreach (var betweenCondition in SearchQuery.BetweenConditions ?? Enumerable.Empty<BetweenCondition>())
+                {
+
+                    betweenCondition.LowerValue = GetValue(betweenCondition.LowerValue, dateFormat);
+                    betweenCondition.UpperValue = GetValue(betweenCondition.UpperValue, dateFormat);
+                }
+
+                foreach (var containCondition in SearchQuery.ContainsConditions ?? Enumerable.Empty<ContainsCondition>())
+                {
+                    ICollection<string> collection = containCondition.Values.Select(value => GetValue(value, dateFormat)).ToList();
+                    containCondition.Values = collection;
+                }
             }
+           
         }
         #endregion
 
+        #region Method
+        protected virtual string GetValue(string key, string dateFormat)
+        {
+            if (ID.TryParse(key, out var id))
+            {
+                var item = Context.Database.GetItem(id);
+                return item?.DisplayName ?? key;
+            }
+
+            if (ShortID.TryParse(key, out var shortId))
+            {
+                var item = Context.Database.GetItem(shortId.ToID());
+                return item?.DisplayName ?? key;
+            }
+
+            if (DateTime.TryParse(key, out var date))
+            {
+                return date.ToLocalTime().ToString(dateFormat);
+            }
+            return key;
+        }
+        #endregion
     }
 }
